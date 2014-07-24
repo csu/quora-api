9af445cc4be5a6164cf9475b06305feaa9c16bba
Christopher Su <christophersu9@gmail.com>
2014-07-23 22:12:16 -0700

    readme

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ quora-api
 =========
 
 An unofficial API for Quora. Currently implemented: Users.
+
+# API
+## GET: `/users/<user>`
+Example usage: GET `/users/Christopher-J-Su`
+
+Example result:
+
+    {
+      "answers": 19, 
+      "blogs": 2, 
+      "edits": 120, 
+      "followers": 6, 
+      "following": 64, 
+      "name": "Christopher J. Su", 
+      "posts": 1, 
+      "questions": 8, 
+      "topics": 270, 
+      "username": "Christopher-J-Su"
+    }
--- server.py
+++ server.py
@@ -1,15 +1,56 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify, request
+import requests
+from bs4 import BeautifulSoup
+import re
 
 app = Flask(__name__)
+
+####################################################################
+# Helpers
+####################################################################
+def get_count(element):
+    try:
+        return int(element.find('span', class_='profile-tab-count').string)
+    except ValueError:
+        return element.find('span', class_='profile-tab-count').string
+
+def get_count_for_user_href(soup, user, suffix):
+    return get_count(soup.find('a', class_='link_label', href='/' + user + '/' + suffix))
 
 ####################################################################
 # Routes
 ####################################################################
 
 @app.route('/', methods=['GET'])
-def index():
+def index_route():
     return jsonify({'author': 'Christopher Su'})
+
+####################################################################
+# Users
+####################################################################
+
+@app.route('/users/<user>', methods=['GET'])
+def user_route(user):
+    soup = BeautifulSoup(requests.get('http://www.quora.com/' + user).text)
+    user_dict = { 'username': user }
+    user_dict['name'] = soup.find('h1').find('span', class_='user').string
+    attributes_to_href_suffix = {
+        'followers': 'followers',
+        'following': 'following',
+        'topics': 'topics',
+        'blogs': 'blogs',
+        'posts': 'all_posts',
+        'questions': 'questions',
+        'answers': 'answers',
+        'edits': 'log'
+    }
+    for attribute, suffix in attributes_to_href_suffix.iteritems():
+        try:
+            user_dict[attribute] = get_count_for_user_href(soup, user, suffix)
+        except:
+            pass
+    return jsonify(user_dict)
 
 ####################################################################
 # Start Flask
