624dbfcb20842afd722dd35ecfb251e21abfdb2a
Christopher Su <christophersu9@gmail.com>
2014-07-26 13:47:11 -0700

    fixing submodules

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify, request
-from pyquora import Quora
+from pyquora import Quora, Activity
 
 app = Flask(__name__)
 
@@ -30,8 +30,24 @@ def user_route(user):
 def user_activity_route(user):
     return jsonify(Quora.get_user_activity(user))
 
+@app.route('/users/<user>/answers', methods=['GET'])
+def user_answers_route(user):
+    return jsonify(Quora.get_activity(user).answers)
+
+@app.route('/users/<user>/questions', methods=['GET'])
+def user_questions_route(user):
+    return jsonify(Quora.get_activity(user).questions)
+
+@app.route('/users/<user>/question_follows', methods=['GET'])
+def user_question_follows_route(user):
+    return jsonify(Quora.get_activity(user).question_follows)
+
+@app.route('/users/<user>/votes', methods=['GET'])
+def user_votes_route(user):
+    return jsonify(Quora.get_activity(user).upvotes)
+
 ####################################################################
 # Start Flask
 ####################################################################
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
